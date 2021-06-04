8b7bd6511d34119d54b0dc4c4f50b96f9170c04f
InvidusS <47109249+InvidusS@users.noreply.github.com>
2021-06-04 23:44:47 +0200

    Update ts1.cpp

--- tests/ts1.cpp
+++ tests/ts1.cpp
@@ -1,1 +1,34 @@
+#include <gtest/gtest.h>
+#include "../src/rational.h"
 
+TEST(TS1, testing_normalized) {
+    Rational result (-35, 33);
+    Rational r = (3 + Rational(2, 11)) * Rational(-1, 3);
+    ASSERT_EQ(result, Rational::normalized(r.getNumerator(), r.getDenominator()));
+}
+
+TEST(TS2, testing_normalize) {
+    Rational result (153, 55);
+    Rational r = 3 + Rational(-1, 3) * (Rational(2, 11) + 2) / (Rational(18, 6) - Rational(1, -3));
+    ASSERT_EQ(result, Rational::normalize(r));
+}
+
+TEST(TS3, testing_equality_operator_class){
+    Rational result(1,2), r1;
+    ASSERT_EQ(result, r1 = result);
+}
+
+TEST(TS4, testing_equality_operator_int){
+    Rational result(4,1), r1;
+    ASSERT_EQ(result, r1 = 4);
+}
+
+TEST(TS5, testing_numerator_getter){
+    Rational r(4,1);
+    ASSERT_EQ(4, r.getNumerator());
+}
+
+TEST(TS6, testing_denominator_getter){
+    Rational r(4,1);
+    ASSERT_EQ(1, r.getDenominator());
+}
